a7ce67b786e67fb4efb8237af10a077709b6e934
DeadlyMoon23 <mac.charry@mail.ru>
2025-04-02 08:28:44 +0500

    Step 24 not found error page

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
 import Spinner from '../spinner'
-import SwapiService from '../services/swapi-service'
-import ErrorIndicator from "../error-indicator"
+import SwapiService from '../../services/swapi-service'
+import { ErrorIndicator } from "../errors"
 
 import './random-planet.css'
 
